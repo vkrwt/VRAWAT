fcd8a3edf1b91d6fabe0833ff94c5384cec99f1e
vkrwt <130211169+vkrwt@users.noreply.github.com>
2023-04-09 09:41:14 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,40 @@
+# Define a function to add numbers
+def add(x, y):
+    return x + y
+
+# Define a function to subtract numbers
+def subtract(x, y):
+    return x - y
+
+# Define a function to multiply numbers
+def multiply(x, y):
+    return x * y
+
+# Define a function to divide numbers
+def divide(x, y):
+    return x / y
+
+print("Please select an operation.")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+# Take input from the user
+choice = input("Enter choice(1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+    print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+    print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+    print(num1,"/",num2,"=", divide(num1,num2))
+else:
+    print("Invalid input")
